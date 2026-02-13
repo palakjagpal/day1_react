d5cdce6d6ecc796325c90a965f107ef260e74188
Palak Jagpal <palakjagpal70@gmail.com>
2026-02-13 15:13:42 +0530

    Refactor question generation logic in Quiz component

--- src/Quiz.jsx
+++ src/Quiz.jsx
@@ -1,32 +1,31 @@
 import {useState} from "react";
 
 function Quiz(){
-    const [questions, setQuestions] = useState([])
+    
+    const [ questions, setQuestions] = useState([])
     const operators = ["+", "-", "*", "/", "%"];
     
     const generateQuestions = () => {
         const a = Math.floor(Math.random() * 101);
         const b = Math.floor(Math.random() * 101);
-        return [
-            {
-                question1 : `What is ${a} ${operators[Math.floor(Math.random()*operators.length)]} ${b} ?`
-            },
-            {
-                question2: `What is ${a} ${operators[Math.floor(Math.random()*operators.length)]} ${b} ?`
-            }                
-        ]
+        const op =operators[Math.floor(Math.random()*operators.length)];
+        console.log(`${a} ${op} ${b}` )
+        const result = eval(`${a} ${op} ${b}`);
+         return result;
+        
     }
-
     return(
         <div>
             <h1>Quiz App</h1>
             <button onClick = {
                 () => {
                     const newQuestions = generateQuestions();
-                    setQuestions(newQuestions);
-                    console.log("Questions are : ", newQuestions);                                                                       
+                  //  setQuestions(newQuestions);
+
+                    console.log("answer : ", newQuestions);
                 }
             }>Start</button>
+    
         </div>
 
     )

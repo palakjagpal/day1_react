b1c668a696d8ba6c7c72b8998f674b8d6a323e43
Palak Jagpal <palakjagpal70@gmail.com>
2026-03-14 22:10:13 +0530

    Updated validation logic

--- src/UserAuth.jsx
+++ src/UserAuth.jsx
@@ -11,15 +11,26 @@ function UserAuth(){
     const [show, setshow] = useState(true);
     const [success, setsuccess] = useState("");
 
+    const email_regex = /^[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,}$/;
+
     const api_url = 'https://user-authentication-mongodb-demo.onrender.com/api/auth/signin'
 
     async function UserLogin(e) {
-        if(email === "" && email.length ===0 && password === "" && password.length ===0 ){
+        e.preventDefault();
+
+        if(email.trim() === "" && password.trim() === ""){
             seterror("Please enter data!");
             console.log("Please enter data!");
-        }else{
-            try{
-            e.preventDefault();
+            return;
+        }
+         
+        if(!email_regex.test(email)){
+            console.log("Please enter a valid email address");
+            seterror("Please enter a valid email address");
+            return;
+        }
+
+        try{
             const response = await axios.post(api_url,{
                 email,
                 password,
@@ -28,41 +39,49 @@ function UserAuth(){
             settoken(response.data.token);
             setsuccess("Login Successfull!")
             if(response.data.token){
-                localStorage.setItem("token",token);
+                localStorage.setItem("token",response.data.token);
                 console.log("Login Successfull!")
                 console.log("Token stored in browser")
             }
             setemail("");
-            setpassword("");   
-            }catch(err){
+            setpassword("");
+        }catch(err){
                 seterror("Invalid Credentials")
                 console.log("Invalid Credentials")
                 console.log("Error : ", err);
                 setemail("");
                 setpassword("");
             }
-        }
     }
 
     return(
         <>
-            {error && <div><p className="error">{error}</p></div>}
+            {error && (<div><p className="error">{error}</p></div>)}
 
-            {success && <div><p className="success">{success}</p></div>}
+            {success && (<div><p className="success">{success}</p></div>)}
 
                 <div className="main">
                     <h1>Welcome Back!</h1>
                     <form onSubmit={UserLogin}>
                         <div className="field">
                             <label>Email</label>
-                            <input type="text" placeholder="example@gmail.com" pattern="[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,}$" title="Please enter a valid email address (e.g., user@domain.com)" value={email} required 
-                            onChange={(e) => setemail(e.target.value)}></input>
+                            <input type="text" placeholder="example@gmail.com"  
+                            value={email}
+                            onChange={(e) => {
+                                setemail(e.target.value); 
+                                seterror(""); 
+                                setsuccess("");
+                            }}></input>
                         </div>
                         <div className="field">
                             <label>Password</label>
-                            <input type={show ? "password" : "text"} placeholder="********" maxLength="8" value={password} 
-                            required 
-                            onChange={(e) => setpassword(e.target.value)}></input>
+                            <input type={show ? "password" : "text"} placeholder="********" 
+                            maxLength="8" value={password} 
+                            onChange={(e) => {
+                                setpassword(e.target.value); 
+                                seterror(""); 
+                                setsuccess("");
+                            }}></input>
                             <i className = { show ? "fa-solid fa-eye" : "fa-solid fa-eye-slash"} 
                             onClick = {() => setshow((s) => !s)}></i>
                         </div>
